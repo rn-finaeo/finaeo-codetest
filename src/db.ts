5bb84970d18b4f2d858063167e1dae8c1c698189
Rohan Nair <59209613+rn-finaeo@users.noreply.github.com>
2020-06-23 13:15:11 -0400

    Initial commit

---
+++ src/db.ts
@@ -0,0 +1,50 @@
+enum UserRole {
+  User,
+  Admin,
+  Super
+}
+
+interface User {
+  id: number;
+  givenName: string;
+  familyName: string;
+  dateOfBirth: string;
+  role: UserRole;
+}
+
+interface Car {
+  id: number;
+  make: string;
+  model: string;
+  user: User;
+}
+
+interface Data {
+  users: User[];
+  cars: Car[];
+}
+
+const DATA: Data = {
+  users: [],
+  cars: []
+};
+
+export const db = (dataType: string, ...opts) => {
+  // You can use the DATA object in here
+  // Begin editing after this line
+  console.log(DATA);
+
+  return {
+    getAllUsers: (): User[] => {},
+    getOneUser: (): User => {},
+    createUser: (): Partial<User> => {},
+    updateUser: (): Partial<User> => {},
+    deleteUser: (): boolean => {},
+    getAllCars: (): Car[] => {},
+    getOneCar: (): Car => {},
+    createCar: (): Partial<Car> => {},
+    updateCar: (): Partial<Car> => {},
+    deleteCar: (): boolean => {}
+  };
+  // End editing after this line
+};
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import Koa from "koa";
+import Router from "koa-router";
+import logger from "koa-logger";
+import bodyParser from "koa-bodyparser";
+
+export const app = new Koa();
+const router = new Router();
+
+router.get("/", async (ctx: Koa.Context) => {
+  const name = ctx.query.name || "stranger";
+  ctx.body = {
+    message: `Hello, ${name}!`
+  };
+});
+
+app
+  .use(logger())
+  .use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods());
